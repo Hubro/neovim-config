6dabe544ee09539a18361eb2b96da5166937f184
Tomas Sandven <tomas@sandven.email>
2023-05-01 17:03:32 +0200

    Improve AutoPairs config

--- init_plugins.lua
+++ init_plugins.lua
@@ -86,6 +86,11 @@ require("lazy").setup({
       vim.g.AutoPairsShortcutFastWrap = "<M-w>"
       vim.g.AutoPairsShortcutJump = ""
       vim.g.AutoPairsShortcutBackInsert = "<M-b>"
+      vim.g.AutoPairsMultilineClose = 0 -- Never auto jump to next line
+      vim.g.AutoPairsMapCh = 0 -- Don't map to <C-h>
+
+      -- Center the viewport when pressing <CR> when near edge of viewport
+      vim.g.AutoPairsCenterLine = 1
     end,
   },
 
