3a8e4a0f35aa4c96ad063e789bd72745aa8bda57
Tomas Sandven <tomas@sandven.email>
2023-07-07 18:06:15 +0200

    Improve markdown settings

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,5 +1,14 @@
-vim.wo.colorcolumn = "121"
-vim.bo.textwidth = 120
+vim.wo.colorcolumn = ""
+vim.wo.wrap = true
+vim.wo.linebreak = true
+
+-- With Markdown, it's often nice to not care about manual line wrapping and
+-- let lines overflow instead. These mapping makes soft-wrapped lines easier to
+-- navigate.
+vim.keymap.set({ "n", "v" }, "j", "gj", { buffer = true })
+vim.keymap.set({ "n", "v" }, "k", "gk", { buffer = true })
+vim.keymap.set({ "n", "v" }, "0", "g0", { buffer = true })
+vim.keymap.set({ "n", "v" }, "$", "g$", { buffer = true })
 
 -- Some cool extra keybinds for Markdown formatting
 vim.keymap.set("v", "<C-b>", "S*gvS*", { buffer = true, remap = true })
