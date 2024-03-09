eb859a0412e5174bc795228d9af25e740683c403
Tomas Sandven <tomas@sandven.email>
2024-03-09 18:42:19 +0100

    Fuck around with python3_host_prog more
    Apparently extremely hard to get right.

--- init.lua
+++ init.lua
@@ -4,18 +4,26 @@
 --vim.env.EDITOR =
 --  "neovide --geometry 100x60 --wayland-app-id neovide-floating --nofork -- +startinsert"
 
-vim.env.PATH = vim.env.HOME .. "/.local/share/nvim/mason/bin:" .. vim.env.PATH
+--vim.env.PATH = vim.env.HOME .. "/.local/share/nvim/mason/bin:" .. vim.env.PATH
 
 local mac_python = "/usr/bin/python3"
 --local mac_python = "/Users/tomas/.pyenv/versions/neovim/bin/python"
-local linux_python = "python3"
+local nixos_python = "/run/current-system/sw/bin/python3"
 
 vim.g.mapleader = " "
 
-if vim.fn.executable(mac_python) == 1 then
+if vim.g.python3_host_prog ~= nil
+    and vim.fn.executable(vim.g.python3_host_prog) == 1 then
+  -- Already set up properly by system, e.g. on NixOS
+elseif vim.fn.executable(mac_python) == 1 then
   vim.g.python3_host_prog = mac_python
+elseif vim.fn.executable(nixos_python) == 1 then
+  vim.g.python3_host_prog = nixos_python
 else
-  vim.g.python3_host_prog = linux_python
+  -- Note: Using a relative path like "python3" will resolve virtualenv
+  -- pythons, which will break nvim integration unless the Neovim provider
+  -- package is installed there. Use the system Python if possible.
+  vim.g.python3_host_prog = "python3"
 end
 
 vim.cmd([[au BufRead,BufNewFile ~/.config/nvim/*.lua set foldmethod=marker]])
