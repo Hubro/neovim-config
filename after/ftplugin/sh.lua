82e0273813b5a3e43f925af9fd07c9b99af0dcfd
Tomas Sandven <tomas@sandven.email>
2024-03-09 18:38:57 +0100

    Add after/ftplugin/sh.lua

---
+++ after/ftplugin/sh.lua
@@ -0,0 +1,6 @@
+-- I don't like this, but apparently it's what Bash demands... Using spaces for
+-- indentation breaks stuff like heredoc syntax.
+vim.bo.expandtab = false
+vim.bo.tabstop = 4
+vim.bo.softtabstop = 0
+vim.bo.shiftwidth = 0
