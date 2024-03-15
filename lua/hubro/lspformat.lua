6d734267b36b4c8399c1aee73de1ab941b7cb4bd
Tomas Sandven <tomas@sandven.email>
2024-03-15 11:19:06 +0100

    Prioritize between LSP servers in lspformat.lua

--- lua/hubro/lspformat.lua
+++ lua/hubro/lspformat.lua
@@ -1,5 +1,39 @@
 local restore_view = require("hubro.restore_view")
 
+local default_priority = 50
+
+local formatting_priorities = {
+  svelte = 100,
+  efm = 80,
+}
+
+-- Selects one LSP client to use for formatting
+local function _select_formatter(lsp_clients)
+  local winner = {}
+  local prio = 0
+
+  for _, client in ipairs(lsp_clients) do
+    local client_name = client.name
+    local client_prio = default_priority
+
+    if formatting_priorities[client_name] ~= nil then
+      client_prio = formatting_priorities[client_name]
+    end
+
+    if client_prio > prio then
+      prio = client_prio
+      winner = { client }
+    elseif client_prio == prio then
+      vim.notify(
+        "More than one connected LSP server has the same formatting priority",
+        vim.log.levels.WARN
+      )
+    end
+  end
+
+  return winner
+end
+
 -- Function for formatting with the first available LSP server
 --
 -- If no active LSP servers support it, do nothing
@@ -11,16 +45,36 @@ local function lspformat()
     return
   end
 
-  local clients = vim.lsp.get_clients({ buffer = 0 })
+  local buf = vim.api.nvim_get_current_buf()
 
-  -- Loop over clients
-  for id, client in pairs(clients) do
-    if client.server_capabilities.documentFormattingProvider then
-      restore_view(function()
-        vim.lsp.buf.format({ id = id })
-      end)
+  local clients = {}
+
+  for _, client in ipairs(vim.lsp.get_clients()) do
+    if vim.lsp.buf_is_attached(buf, client.id)
+        and client.supports_method("textDocument/formatting")
+        and client.server_capabilities.documentFormattingProvider then
+      table.insert(clients, client)
     end
   end
+
+  if #clients == 0 then
+    vim.notify("No attached LSP clients supports textDocument/formatting")
+    return
+  end
+
+  if #clients > 1 then
+    clients = _select_formatter(clients)
+  end
+
+  for _, client in pairs(clients) do
+    restore_view(function()
+      -- Use a long timeout, in case we're using "nix run" and we've just
+      -- garbage collected or something
+      vim.lsp.buf.format({ id = client.id, timeout_ms = 30000 })
+    end)
+  end
 end
+
+lspformat()
 
 return lspformat
