7ec9038543f4f829abc27af3022df19a9d366186
Tomas Sandven <tomas@sandven.email>
2024-03-17 14:45:22 +0100

    Improve copilot keymaps

--- init_plugins.lua
+++ init_plugins.lua
@@ -268,15 +268,41 @@ require("lazy").setup({
     "github/copilot.vim",
     init = function()
       -- Keep copilot disabled, only invoke explicitly
-      --vim.g.copilot_filetypes = { ["*"] = false }
+      vim.g.copilot_filetypes = { ["*"] = false }
+      vim.g.copilot_no_tab_map = true
+
+      local _plug_accept = function(thing)
+        local expr = vim.api.nvim_replace_termcodes(
+          "<Plug>(copilot-accept-" .. thing .. ")", true, true, true
+        )
+
+        vim.api.nvim_feedkeys(expr, "m", false)
+      end
 
       local suggest = function()
         require("cmp").abort() -- Hide cmp completion window
         vim.fn["copilot#Suggest"]()
       end
 
-      vim.keymap.set("n", "<Leader>cp", ":Copilot panel<CR>", { silent = true })
+      local accept_word = function()
+        _plug_accept("word")
+
+        vim.schedule(suggest)
+      end
+
+      local accept_line = function()
+        _plug_accept("line")
+
+        vim.schedule(suggest)
+      end
+
       vim.keymap.set("i", "<A-p>", suggest, { silent = true })
+      vim.keymap.set("i", "<C-e>", 'copilot#Accept("")', { expr = true, replace_keycodes = false })
+      vim.keymap.set("i", "<A-l>", accept_word)
+      vim.keymap.set("i", "<A-h>", accept_line)
+      -- vim.keymap.set("i", "<A-l>", "<Plug>(copilot-accept-word)", { remap = true })
+      -- vim.keymap.set("i", "<A-h>", "<Plug>(copilot-accept-line)", { remap = true })
+      vim.keymap.set("n", "<Leader>cp", ":Copilot panel<CR>", { silent = true })
       vim.keymap.set("i", "<A-j>", "<Plug>(copilot-next)", { silent = true })
       vim.keymap.set(
         "i",
