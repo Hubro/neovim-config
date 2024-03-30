0a1c2f8ca20f7d8883724dc2e8e150b363f5b735
Tomas Sandven <tomas@sandven.email>
2024-03-30 15:55:05 +0100

    Remove unnecessary pcall wrapper in setup_cmp.lua

--- setup_cmp.lua
+++ setup_cmp.lua
@@ -1,5 +1,5 @@
-local installed, cmp = pcall(require, "cmp")
-local lspkind_installed, lspkind = pcall(require, "lspkind")
+local cmp = require("cmp")
+local lspkind = require("lspkind")
 
 local buffer = function(opts)
   if not opts then
@@ -20,104 +20,98 @@ local buffer = function(opts)
   return source
 end
 
-if installed then
-  types = require("cmp.types")
+types = require("cmp.types")
 
-  local MAPPING = {
-    ["<C-n>"] = {
-      i = cmp.mapping.select_next_item({ behavior = types.cmp.SelectBehavior.Insert }),
+local MAPPING = {
+  ["<C-n>"] = {
+    i = cmp.mapping.select_next_item({ behavior = types.cmp.SelectBehavior.Insert }),
+  },
+  ["<C-p>"] = {
+    i = cmp.mapping.select_prev_item({ behavior = types.cmp.SelectBehavior.Insert }),
+  },
+  ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+  ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+  ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+  ["<Right>"] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+}
+
+vim.opt.completeopt = "menu,menuone,noselect"
+
+-- https://github.com/onsails/lspkind.nvim
+formatting = {
+  format = lspkind.cmp_format({
+    with_text = true,
+    menu = {
+      buffer = "[buf]",
+      nvim_lsp = "[LSP]",
+      nvim_lua = "[api]",
+      path = "[path]",
+      ultisnips = "[snip]",
     },
-    ["<C-p>"] = {
-      i = cmp.mapping.select_prev_item({ behavior = types.cmp.SelectBehavior.Insert }),
-    },
-    ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-    ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-    ["<Right>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-  }
+  }),
+}
 
-  vim.opt.completeopt = "menu,menuone,noselect"
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["UltiSnips#Anon"](args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert(MAPPING),
+  sources = {
+    { name = "ultisnips", group_index = 1 },
+    { name = "nvim_lsp",  group_index = 1 },
 
-  local formatting = {}
-
-  if lspkind_installed then
-    -- https://github.com/onsails/lspkind.nvim
-    formatting = {
-      format = lspkind.cmp_format({
-        with_text = true,
-        menu = {
-          buffer = "[buf]",
-          nvim_lsp = "[LSP]",
-          nvim_lua = "[api]",
-          path = "[path]",
-          ultisnips = "[snip]",
-        },
-      }),
-    }
-  end
-
-  cmp.setup({
-    snippet = {
-      expand = function(args)
-        vim.fn["UltiSnips#Anon"](args.body)
-      end,
-    },
-    mapping = cmp.mapping.preset.insert(MAPPING),
-    sources = {
-      { name = "ultisnips", group_index = 1 },
-      { name = "nvim_lsp",  group_index = 1 },
-
-      {
-        name = "buffer",
-        group_index = 2,
-        option = {
-          keyword_pattern = [[\k\+]],
-          get_bufnrs = function()
-            return vim.api.nvim_list_bufs()
-          end
-        },
-      },
-      { name = "path", group_index = 2 },
-
-      --{ name = "buffer",    group_index = 2, option = { keyword_pattern = [[\k\+]] } },
-      --{ name = "path",      group_index = 2 },
-
-      -- If nothing else matches, complete words from other buffers
-      --{
-      --  name = "buffer",
-      --  group_index = 3,
-      --  option = {
-      --    keyword_pattern = [[\k\+]],
-      --    get_bufnrs = function()
-      --      return vim.api.nvim_list_bufs()
-      --    end
-      --  },
-      --},
-    },
-    formatting = formatting,
-    experimental = {
-      native_menu = false,
-      ghost_text = true,
-    },
-  })
-
-  cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = "cmdline" },
-    }),
-    completion = {
-      keyword_length = 1,
-    },
-  })
-
-  cmp.setup.cmdline("/", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-      {
-        buffer({ all_buffers = true }),
+    {
+      name = "buffer",
+      group_index = 2,
+      option = {
+        keyword_pattern = [[\k\+]],
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
+        end
       },
     },
-  })
-end
+    { name = "path", group_index = 2 },
+
+    --{ name = "buffer",    group_index = 2, option = { keyword_pattern = [[\k\+]] } },
+    --{ name = "path",      group_index = 2 },
+
+    -- If nothing else matches, complete words from other buffers
+    --{
+    --  name = "buffer",
+    --  group_index = 3,
+    --  option = {
+    --    keyword_pattern = [[\k\+]],
+    --    get_bufnrs = function()
+    --      return vim.api.nvim_list_bufs()
+    --    end
+    --  },
+    --},
+  },
+  formatting = formatting,
+  experimental = {
+    native_menu = false,
+    ghost_text = true,
+  },
+})
+
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "cmdline" },
+  }),
+  completion = {
+    keyword_length = 1,
+  },
+})
+
+cmp.setup.cmdline("/", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    {
+      buffer({ all_buffers = true }),
+    },
+  },
+})
