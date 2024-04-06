a7df0178272f861fb9dde59f3a12fc7e49d7bf1c
Tomas Sandven <tomas@sandven.email>
2024-04-07 00:02:33 +0200

    Add aerial- and indents queries for robot

---
+++ queries/robot/aerial.scm
@@ -0,0 +1,24 @@
+
+(settings_section
+  (section_header) @name
+  (#set! "kind" "Namespace")) @symbol
+
+(variables_section
+  (section_header) @name
+  (#set! "kind" "Namespace")) @symbol
+
+(keywords_section
+  (section_header) @name
+  (#set! "kind" "Namespace")) @symbol
+
+(keyword_definition
+  (name) @name
+  (#set! "kind" "Function")) @symbol
+
+(test_cases_section
+  (section_header) @name
+  (#set! "kind" "Namespace")) @symbol
+
+(test_case_definition
+  (name) @name
+  (#set! "kind" "Method")) @symbol
---
+++ queries/robot/indents.scm
@@ -0,0 +1,5 @@
+(section_header) @indent.zero
+(keyword_definition (name) @indent.zero)
+(keyword_definition (name) @indent.begin)
+(test_case_definition (name) @indent.zero)
+(test_case_definition (name) @indent.begin)
