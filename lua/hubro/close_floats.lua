5b5ade4f232112ed4022a540283dd86b55f5a578
Tomas Sandven <tomas@sandven.email>
2024-04-07 00:01:41 +0200

    Improve close_floats

--- lua/hubro/close_floats.lua
+++ lua/hubro/close_floats.lua
@@ -1,16 +1,35 @@
--- Closes all non-focused floating windows
-return function()
-  local inactive_floating_wins = vim.fn.filter(
-    vim.api.nvim_list_wins(),
-    function(_, v)
-      -- local buf = vim.api.nvim_win_get_buf(v)
-      -- local file_type = vim.api.nvim_buf_get_option(buf, "filetype")
+--
+-- Closes all non-focused short-lived floating windows
+--
+-- Should be bound to Esc key to help clear the viewport of distractions such
+-- as LSP popups.
+--
+local close_floats = function()
+  for _, w in ipairs(vim.api.nvim_list_wins()) do
+    local window_config = vim.api.nvim_win_get_config(w)
+    local is_floating = window_config.relative ~= ""
+    local is_focused = vim.api.nvim_get_current_win() == w
 
-      return vim.api.nvim_win_get_config(v).relative ~= ""
-        and v ~= vim.api.nvim_get_current_win()
+    local buf = vim.api.nvim_win_get_buf(w)
+    local first_line = vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1]
+
+    -- Let's not kill borders and scroll bars
+    local is_decoration = (
+      window_config.width == 1
+      or window_config.height == 1
+      -- Detects floaterm border:
+      or string.find(first_line, "────────────────────")
+    )
+
+    -- local is_floaterm_border =
+
+    if is_floating and not is_focused and not is_decoration then
+      print("KILLING WINDOW:")
+      print(vim.inspect(window_config))
+      print(vim.inspect(first_line))
+      pcall(vim.api.nvim_win_close, w, false)
     end
-  )
-  for _, w in ipairs(inactive_floating_wins) do
-    pcall(vim.api.nvim_win_close, w, false)
   end
 end
+
+return close_floats
