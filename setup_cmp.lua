f3b58d782041114e0b04aae7e93f555fc896b808
Tomas Sandven <tomas@sandven.email>
2024-04-14 19:10:03 +0200

    Add custom comparator for nvim-cmp
    This lets me customize completion item sort order for LSP suggestions.

--- setup_cmp.lua
+++ setup_cmp.lua
@@ -20,7 +20,7 @@ local buffer = function(opts)
   return source
 end
 
-types = require("cmp.types")
+local types = require("cmp.types")
 
 local MAPPING = {
   ["<C-n>"] = {
@@ -38,19 +38,94 @@ local MAPPING = {
 
 vim.opt.completeopt = "menu,menuone,noselect"
 
--- https://github.com/onsails/lspkind.nvim
-formatting = {
-  format = lspkind.cmp_format({
-    with_text = true,
-    menu = {
-      buffer = "[buf]",
-      nvim_lsp = "[LSP]",
-      nvim_lua = "[api]",
-      path = "[path]",
-      ultisnips = "[snip]",
-    },
-  }),
-}
+--- My custom sort order scoring for LSP completion items
+---
+--- A higher score means it should go higher up in the list.
+---
+---@type fun(client: vim.lsp.Client, item: lsp.CompletionItem): integer
+local score_lsp_completion_item = function(client, item)
+  local score = 0
+  local kind = vim.lsp.protocol.CompletionItemKind[item.kind]
+
+  local kind_score = {
+    EnumMember = 20,
+    Field = 10,
+    Method = 10,
+    Property = 10,
+    Snippet = 10,
+    Constant = 5,
+    Function = 5,
+    Keyword = 5,
+    TypeParameter = 5,
+    Value = 5,
+    Variable = 5,
+    Class = 0,
+    Color = 0,
+    Constructor = 0,
+    Enum = 0,
+    Event = 0,
+    File = 0,
+    Folder = 0,
+    Interface = 0,
+    Module = 0,
+    Operator = 0,
+    Reference = 0,
+    Struct = 0,
+    Text = 0,
+    Unit = 0,
+  }
+
+  score = score + (kind_score[kind] or 0)
+
+  if client.name == "rust_analyzer" then
+    -- If the completion item requires an import, it should be at the bottom
+    if item.data ~= nil and item.data.imports ~= nil then
+      score = score - 100
+    end
+  end
+
+  if client.name == "pyright" then
+    -- Hidden properties should have lower prio
+    if item.label:match("^_") then
+      score = score - 1
+    end
+
+    -- Mangled properties should have even lower prio
+    if item.label:match("^__") then
+      score = score - 1
+    end
+  end
+
+  return score
+end
+
+--- Custom sort order for completion entries
+---
+--- Should return "true" if entry1 should be higher up in the list than
+--- entry2. Otherwise should return "nil".
+---
+---@type fun(entry1: cmp.Entry, entry2: cmp.Entry): boolean|nil
+local cmp_lsp_custom = function(entry1, entry2)
+  if entry1.source.name ~= "nvim_lsp" or entry2.source.name ~= "nvim_lsp" then
+    return nil
+  end
+
+  local lspclient1 = entry1.source.source.client
+  local item1 = entry1:get_completion_item()
+  local score1 = score_lsp_completion_item(lspclient1, item1)
+
+  local lspclient2 = entry2.source.source.client
+  local item2 = entry2:get_completion_item()
+  local score2 = score_lsp_completion_item(lspclient2, item2)
+
+  if score1 > score2 then
+    return true
+  elseif score2 > score1 then
+    return false
+  else
+    return nil
+  end
+end
 
 cmp.setup({
   snippet = {
@@ -91,10 +166,32 @@ cmp.setup({
     --  },
     --},
   },
-  formatting = formatting,
+  sorting = {
+    priority_weight = 1,
+    comparators = {
+      cmp.config.compare.exact,
+      cmp_lsp_custom,
+      cmp.config.compare.scopes,
+      cmp.config.compare.locality,
+      cmp.config.compare.sort_text,
+    },
+  },
+  ---@diagnostic disable-next-line: missing-fields
+  formatting = {
+    format = lspkind.cmp_format({
+      with_text = true,
+      menu = {
+        buffer = "[buf]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[api]",
+        path = "[path]",
+        ultisnips = "[snip]",
+      },
+    }),
+  },
   experimental = {
     native_menu = false,
-    ghost_text = true,
+    ghost_text = false, -- Conflicts with copilot.nvim
   },
 })
 
