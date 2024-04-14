9549abf05f81e25a592b33f4fb28965726a648b6
Tomas Sandven <tomas@sandven.email>
2024-04-15 00:06:29 +0200

    Update nvim-projects config

--- lua/plugins/nvim-projects.lua
+++ lua/plugins/nvim-projects.lua
@@ -10,13 +10,17 @@ return {
     init_filename = ".nvimrc.lua",
     project_dirs = {
       "~/projects",
+      "~/syncthing/dotfiles",
       "~/Dropbox/Projects",
       "~/Dropbox/Avesta/Inhouse projects",
       "~/Dropbox/Avesta Projects",
       "~/Dropbox/Telia/src/github",
     },
+    exclude = {
+      "/.stversions/",
+    },
     after_jump = function(_)
-      -- ...
+      -- TODO: Restore session
     end,
   },
 }
