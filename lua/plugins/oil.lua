049fd31307651bfc47c656a09a6a537f43a6233f
Tomas Sandven <tomas@sandven.email>
2024-04-15 11:22:53 +0200

    Update oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,11 +2,14 @@ return {
   "stevearc/oil.nvim",
   opts = {
     default_file_explorer = true,
+    skip_confirm_for_simple_edits = true,
     keymaps = {
       ["<C-x>"] = "actions.select_split",
       ["<C-v>"] = "actions.select_vsplit",
       ["<C-h>"] = false,
       ["<C-l>"] = false,
+      ["yp"] = "actions.copy_entry_path",
+      ["<Leader>yp"] = function() vim.cmd.normal('"+yp') end,
     },
   },
 }
