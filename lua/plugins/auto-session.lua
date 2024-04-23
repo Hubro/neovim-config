edc0119139832885529186458e006b915bdb61f5
Tomas Sandven <tomas@sandven.email>
2024-04-23 11:45:28 +0200

    Auto-save session regularly
    This is very useful for me since I regularly have to restart nvim after
    crashes.

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -4,14 +4,25 @@ return {
   init = function()
     -- Ref: https://github.com/rmagatti/auto-session?tab=readme-ov-file#recommended-sessionoptions-config
     vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+    local group = vim.api.nvim_create_augroup("AutoSaveSession", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufEnter" }, {
+      group = group,
+      callback = function()
+        local as = require("auto-session")
+        if as.session_exists_for_cwd() then
+          as.AutoSaveSession(nil)
+        end
+      end
+    })
   end,
   opts = {
     auto_session_create_enabled = true,
     auto_save_enabled = true,
     auto_restore_enabled = true,
     auto_session_use_git_branch = true,
+    auto_session_suppress_dirs = { "~/", "~/Desktop", "~/bin" },
     pre_save_cmds = {
-      "tabdo Neotree close",
       function()
         -- ...
       end
@@ -25,4 +36,8 @@ return {
       load_on_setup = true,
     },
   },
+  config = function(_, opts)
+    require("auto-session").setup(opts)
+    require("telescope").load_extension("session-lens")
+  end
 }
