130302a866f3eedbd6cfaa3901b00e160dd5e2df
Tomas Sandven <tomas@sandven.email>
2024-04-23 11:30:03 +0200

    Automatically restart LSP servers on DirenvLoaded
    This fixes Pyright not finding libraries because it was started before
    direnv activated a virtualenv.

--- lua/plugins/direnv.lua
+++ lua/plugins/direnv.lua
@@ -1,8 +1,26 @@
 return {
   "direnv/direnv.vim",
-  commit = "ab2a7e08dd630060cd81d7946739ac7442a4f269",
   init = function()
-    -- Set options here
-    -- vim.g...
+    vim.g.direnv_silent_load = true
+
+    local cached_direnv_diff = nil
+
+    local group = vim.api.nvim_create_augroup("DirenvLoaded", { clear = true })
+    vim.api.nvim_create_autocmd({ "User" }, {
+      group = group,
+      pattern = "DirenvLoaded",
+      callback = function()
+        if vim.env.DIRENV_DIFF ~= cached_direnv_diff then
+          cached_direnv_diff = vim.env.DIRENV_DIFF
+
+          vim.notify("Loaded environment from direnv")
+
+          if vim.cmd.LspRestart ~= nil then
+            vim.notify("Restarting LSP servers")
+            vim.cmd.LspRestart()
+          end
+        end
+      end
+    })
   end
 }
