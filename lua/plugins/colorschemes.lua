cf606de470c6f5de57517c2f9eeb82cb812ba5b6
Tomas Sandven <tomas@sandven.email>
2024-05-05 10:46:45 +0200

    Update colorschemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,15 +3,15 @@ return {
   --"tomasr/molokai", -- Molokai
   --"arcticicestudio/nord-vim", -- Nord
   --"cocopon/iceberg.vim", -- Iceberg
-  --"morhetz/gruvbox", -- Gruvbox
-  --"lifepillar/vim-gruvbox8", -- Gruvbox (Simplified and optimized)
   --"ayu-theme/ayu-vim",        -- Ayu
   --"Shatur/neovim-ayu", -- Ayu (Optimized for Neovim)
-  --"folke/tokyonight.nvim", -- TokyoNight
+  "folke/tokyonight.nvim", -- TokyoNight
   --"dracula/vim", -- Dracula
   --"NLKNguyen/papercolor-theme", -- PaperColor
   "EdenEast/nightfox.nvim",                                                          -- Nightfox
   { "catppuccin/nvim",       name = "catppuccin" },                                  -- Catppuccin
   { "rebelot/kanagawa.nvim", opts = { dimInactive = true, terminalColors = true } }, -- Kanagawa
   "AlexvZyl/nordic.nvim",                                                            -- Nordic
+  "ellisonleao/gruvbox.nvim",
+  "kepano/flexoki-neovim",
 }
