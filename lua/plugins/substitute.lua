23b05817714495ce69ad5cd5c92281a915d8e466
Tomas Sandven <tomas@sandven.email>
2024-05-18 13:58:46 +0200

    Add plugin: substitute

---
+++ lua/plugins/substitute.lua
@@ -0,0 +1,13 @@
+return {
+  "gbprod/substitute.nvim",
+  init = function()
+    local sub = require("substitute")
+    vim.keymap.set("n", "s", sub.operator)
+    vim.keymap.set("n", "ss", sub.line)
+    -- vim.keymap.set("n", "S", sub.eol)
+    vim.keymap.set("x", "s", sub.visual)
+  end,
+  opts = {
+    -- ...
+  },
+}
