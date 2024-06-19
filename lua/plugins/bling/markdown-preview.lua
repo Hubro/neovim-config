a2a1cef760ec0b14d42a79e4de1e05f642e9f12e
Tomas Sandven <tomas@sandven.email>
2024-06-19 10:38:20 +0200

    Update markdown-preview plugin config
    For some reason it doesn't auto-open my browser...

--- lua/plugins/bling/markdown-preview.lua
+++ lua/plugins/bling/markdown-preview.lua
@@ -1,1 +1,11 @@
-return { "iamcco/markdown-preview.nvim", build = "cd app && yarn install" }
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreview", "MarkdownPreviewToggle", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function() vim.fn["mkdp#util#install"]() end,
+  init = function()
+    vim.g.mkdp_browser = "/run/current-system/sw/bin/vivaldi"
+    vim.g.mkdp_port = 32080
+    vim.g.mkdp_echo_preview_url = 1
+  end
+}
