8f9fa01302f1bafcf4d169b0c8aa06eded9832eb
Tomas Sandven <tomas@sandven.email>
2024-06-21 11:29:26 +0200

    Fix oil keymaps

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,17 +6,40 @@ return {
     skip_confirm_for_simple_edits = true,
     cleanup_delay_ms = 120 * 1000, -- Wait 2 min before deleting hidden oil buffers
     keymaps = {
-      ["<Tab>"] = "actions.preview",
+      ["π"] = "actions.preview", -- Altgr+P
       ["<C-x>"] = "actions.select_split",
       ["<C-v>"] = "actions.select_vsplit",
       ["<C-p>"] = false,
       ["<C-h>"] = false,
       ["<C-l>"] = false,
       ["gt"] = "actions.open_terminal",
-      ["yp"] = "actions.copy_entry_path",
-      ["<Leader>yp"] = function() vim.cmd.normal('"+yp') end,
     },
   },
+  init = function()
+    local oil = require("oil")
+    local actions = require("oil.actions")
+
+    -- For some reason the keymaps option is really f***ing buggy so I'm
+    -- setting them with an autocommand instead.
+    local group = vim.api.nvim_create_augroup("OilCustomization", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      group = group,
+      pattern = "oil",
+      callback = function()
+        vim.keymap.set("n", "yp", function()
+          actions.copy_entry_path.callback()
+          print(
+            "Copied path to register "
+            .. vim.v.register
+            .. ": "
+            .. vim.fn.getreg(vim.v.register)
+          )
+        end, { buffer = true })
+        vim.keymap.set("n", "<Leader>yp", '"+yp', { buffer = true, remap = true })
+        vim.keymap.set("n", "<Leader>-", actions.parent.callback, { buffer = true })
+      end
+    })
+  end,
   config = function(_, opts)
     require("oil").setup(opts)
 
