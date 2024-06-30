4135f65da6c6f49d9a11f7128a76a6f961db69d1
Tomas Sandven <tomas@sandven.email>
2024-06-30 17:11:30 +0200

    Fix kanagawa lazy loading issue, somehow (???)

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,10 +1,10 @@
 return {
   { "folke/tokyonight.nvim",    lazy = true },
   { "EdenEast/nightfox.nvim",   lazy = true },
-  { "catppuccin/nvim",          lazy = true, name = "catppuccin" },
-  { "rebelot/kanagawa.nvim",    lazy = true, opts = { dimInactive = true, terminalColors = true } },
+  { "catppuccin/nvim",          lazy = true,  name = "catppuccin" },
+  { "rebelot/kanagawa.nvim",    lazy = false, priority = 1000,    opts = { dimInactive = true, terminalColors = true }, config = true },
   { "AlexvZyl/nordic.nvim",     lazy = true },
   { "ellisonleao/gruvbox.nvim", lazy = true },
   { "kepano/flexoki-neovim",    lazy = true },
-  { "Shatur/neovim-ayu",        lazy = true, priority = 1000 },
+  { "Shatur/neovim-ayu",        lazy = false, priority = 1000 },
 }
