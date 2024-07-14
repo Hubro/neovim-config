de427ca73a6b79b200dcd9945a8cd10aae8a4785
Tomas Sandven <tomas@sandven.email>
2024-07-14 16:21:03 +0200

    Further customization of obsidian.nvim

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,3 +1,6 @@
+local health_log_path = "Health log"
+local journal_path = "Journal/Entries"
+
 return {
   "epwalsh/obsidian.nvim",
   version = "*",
@@ -28,18 +31,52 @@ return {
     "ObsidianPasteImg",
     "ObsidianRename",
     "ObsidianToggleCheckbox",
+    "ObsidianNewFromTemplate",
+    "ObsidianTOC",
   },
   init = function()
-    vim.keymap.set("n", "got", ":ObsidianToday<CR>")
-    vim.keymap.set("n", "<Bar>of", ":ObsidianQuickSwitch<CR>")
-    vim.keymap.set("n", "<Bar>os", ":ObsidianSearch<CR>")
+    vim.o.conceallevel = 2 -- So ** and such can be hidden
+
+    -- Prefix "n" for "notes", "o" was taken...
+    vim.keymap.set("n", "<Leader>nt", ":ObsidianToday<CR>")
+    vim.keymap.set("n", "<Leader>nj", function()
+      local client = require("obsidian").get_client()
+      --- @type obsidian.Note
+      local note = client:create_note {
+        title = "Journal entry - " .. os.date("%Y-%m-%d %A"),
+        id = os.date("%Y-%m-%d - %A"),
+        dir = journal_path,
+        template = "Journal entry.md",
+        no_write = true,
+      }
+
+      if not note:exists() then
+        client:write_note(note)
+      end
+
+      client:open_note(note)
+    end)
+    vim.keymap.set("n", "<Bar>nf", ":ObsidianQuickSwitch<CR>")
+    vim.keymap.set("n", "<Bar>ns", ":ObsidianSearch<CR>")
   end,
   opts = {
     callbacks = {
-      enter_node = function()
-        vim.bo.conceallevel = 2
+      enter_note = function()
         vim.bo.tabstop = 4
       end
+    },
+    new_notes_location = "0 - 🏠 Inbox",
+    templates = {
+      substitutions = {
+        health_log_path = health_log_path,
+        journal_path = journal_path,
+        date_with_weekday = function()
+          return os.date("%Y-%m-%d %A")
+        end,
+      },
+    },
+    attachments = {
+      img_folder = "Attachments", -- Ugh, this is relative to the vault root...
     },
     workspaces = {
       {
@@ -51,15 +88,20 @@ return {
             date_format = "%Y-%m-%d - %A",
             template = "Health log entry.md",
           },
+          templates = {
+            folder = "~/Documents/Obsidian/Personal/Templates",
+          },
         },
       },
       {
         name = "Telia",
         path = "~/Documents/Obsidian/Telia",
+        overrides = {
+          templates = {
+            folder = "~/Documents/Obsidian/Telia/Templates",
+          },
+        },
       },
-    },
-    templates = {
-      folder = "Templates",
     },
   },
 }
