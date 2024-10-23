a1b6b391b429f19af08a4524beb0910385442eb2
Tomas Sandven <tomas@sandven.email>
2024-10-23 14:28:38 +0200

    Start Copilot LSP automatically when needed

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,7 +6,25 @@ return {
     vim.g.copilot_filetypes = { ["*"] = false }
     vim.g.copilot_no_tab_map = true
 
+    -- Once it's started, it's started. There's probably never a point in
+    -- checking more than once.
+    local copilot_lsp_started = false
+
     local suggest = function()
+      if not copilot_lsp_started then
+        local clients = vim.tbl_map(
+          function(client) return client.name end,
+          vim.lsp.get_clients()
+        )
+
+        if not vim.tbl_contains(clients, "GitHub Copilot") then
+          vim.notify("Starting GitHub Copilot LSP", "info", { title = "GitHub Copilot" })
+          vim.cmd("silent Copilot restart")
+        end
+
+        copilot_lsp_started = true
+      end
+
       require("cmp").abort() -- Hide cmp completion window
       vim.fn["copilot#Suggest"]()
     end
@@ -29,14 +47,9 @@ return {
     vim.keymap.set("i", "<A-p>", suggest, { silent = true })
     vim.keymap.set("i", "<C-e>", accept, { expr = true, replace_keycodes = false })
     vim.keymap.set("i", "<A-l>", accept_word, { expr = true, remap = true, replace_keycodes = true })
-    vim.keymap.set("i", "<A-h>", accept_line, { expr = true, remap = true, replace_keycodes = true })
+    vim.keymap.set("i", "<A-L>", accept_line, { expr = true, remap = true, replace_keycodes = true })
     vim.keymap.set("n", "<Leader>cp", ":Copilot panel<CR>", { silent = true })
     vim.keymap.set("i", "<A-j>", "<Plug>(copilot-next)", { silent = true })
-    vim.keymap.set(
-      "i",
-      "<A-k>",
-      "<Plug>(copilot-previous)",
-      { silent = true }
-    )
+    vim.keymap.set("i", "<A-k>", "<Plug>(copilot-previous)", { silent = true })
   end,
 }
