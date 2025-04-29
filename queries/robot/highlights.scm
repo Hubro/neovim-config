6d312497f630e0fa14981dabda183cfdfdf5a91b
Tomas Sandven <tomas@sandven.email>
2025-04-29 18:18:54 +0200

    Update custom Robot TS highlight query

--- queries/robot/highlights.scm
+++ queries/robot/highlights.scm
@@ -1,54 +1,47 @@
 
-(comment) @comment
+; Highlight text chunks in "Documentation" settings with @spell
 
-(section_header) @keyword
-(extra_text) @comment
+(setting_statement
+  name: (setting_name) @_name
+  (arguments
+    (argument
+      (text_chunk) @spell))
+  (#match? @_name "^Documentation$"))
 
-(setting_statement) @keyword
+(setting_statement
+  name: (setting_name) @_name
+  (arguments
+    (continuation
+      (argument
+        (text_chunk) @spell)))
+  (#match? @_name "^Documentation$"))
 
-(variable_definition (variable_name) @variable)
+(keyword_setting
+  name: (keyword_setting_name) @_name
+  (arguments
+    (argument
+      (text_chunk) @spell))
+  (#match? @_name "^Documentation$"))
 
-(keyword_definition (name) @function)
-(keyword_definition (body (keyword_setting) @keyword))
+(keyword_setting
+  name: (keyword_setting_name) @_name
+  (arguments
+    (continuation
+      (argument
+        (text_chunk) @spell)))
+  (#match? @_name "^Documentation$"))
 
-(test_case_definition (name) @property)
+(test_case_setting
+  name: (test_case_setting_name) @_name
+  (arguments
+    (argument
+      (text_chunk) @spell))
+  (#match? @_name "^Documentation$"))
 
-(keyword_invocation (keyword) @function.call)
-(ellipses) @punctuation.delimiter
-
-(argument (text_chunk) @string)
-(argument (scalar_variable) @variable)
-(argument (list_variable) @variable)
-(argument (dictionary_variable) @variable)
-(argument (inline_python_expression) @string.special)
-
-(ellipses) @punctuation.delimiter
-
-; Control structures
-(for_statement
-  "FOR" @repeat
-  "END" @repeat)
-(for_statement (in "IN" @repeat))
-(for_statement (in_range "IN RANGE" @repeat))
-(for_statement (in_enumerate "IN ENUMERATE" @repeat))
-(for_statement (in_zip "IN ZIP" @repeat))
-
-(while_statement
-  "WHILE" @repeat
-  "END" @repeat)
-
-(break_statement) @repeat
-(continue_statement) @repeat
-
-(if_statement
-  "IF" @conditional
-  "END" @conditional)
-(if_statement (elseif_statement "ELSE IF" @conditional))
-(if_statement (else_statement "ELSE" @conditional))
-
-(try_statement
-  "TRY" @exception
-  "END" @exception)
-(try_statement (except_statement "EXCEPT" @exception))
-(try_statement (else_statement "ELSE" @exception))
-(try_statement (finally_statement "FINALLY" @exception))
+(test_case_setting
+  name: (test_case_setting_name) @_name
+  (arguments
+    (continuation
+      (argument
+        (text_chunk) @spell)))
+  (#match? @_name "^Documentation$"))
