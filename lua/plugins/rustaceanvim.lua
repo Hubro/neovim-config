6fcc0f129319565acd8879224bdc656e73e16c5a
Tomas Sandven <tomas@sandven.email>
2025-12-01 19:26:32 +0100

    Replace rust-tools with rustaceanvim

---
+++ lua/plugins/rustaceanvim.lua
@@ -0,0 +1,32 @@
+-- rust-tools.nvim - Auto-setup of LSP and debugging + nice commands
+return {
+  "mrcjkb/rustaceanvim",
+  version = '^6',
+  ft = "rust",
+  init = function()
+    vim.g.rustaceanvim = {
+      tools = {
+        -- ...
+      },
+      server = {
+        default_settings = {
+          ["rust-analyzer"] = {
+            -- ...
+          },
+        },
+        on_attach = function(client, bufnr)
+          _G.lsp_on_attach(client, bufnr)
+
+          local opts = { noremap = true, silent = true, buffer = bufnr }
+          vim.keymap.set("n", "K", ":RustLsp hover actions<CR>", opts)
+          vim.keymap.set("n", "<Leader>rr", ":RustLsp runnables<CR>", opts)
+          vim.keymap.set("n", "<Leader>re", ":RustLsp expandMacro<CR>", opts)
+          vim.keymap.set("n", "<Leader>rc", ":RustLsp openCargo<CR>", opts)
+        end,
+      },
+      dap = {
+        -- ...
+      },
+    }
+  end,
+}
